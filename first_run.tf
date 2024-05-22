b88a5cc69137dcf2aed4fa18c464e72843db97d6
sgtslaughta <richardsoto1010@gmail.com>
2024-05-22 12:46:29 -0400

    First run

--- first_run.tf
+++ first_run.tf
@@ -34,15 +34,16 @@ variable "instance_type" {
   default = "t2.micro" # Change to your desired instance type
 }
 
-variable "key_name" {
-  default = "key-048d16352d52cf988" # Change to your key pair name
+resource "aws_key_pair" "deployer" {
+  key_name   = "deployer-key"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCi1FNt4NMX5w/s/KhDFVSEPsWrPLivQuvMp64Ze5ycmEEHcl6jrmwpvAROQSSefZhDPOm+EyOWHwvv/+oPBJm8YmKWXUtwbFNdXYbq5a+8syvmxbsQfHdUdNMhlWbc1logxFbUL0EiDJ+krJIub8KlIGTULbvpo8DsKHNJ0LhyJ3U3fZaKR/vGZM6IpvhJCqORnH2aa7M1qnoOj59U3jSjEVWItjLIF5FyzWpLUS67QseHwPK7IGjw6Z/AGcilFuF0O/gCOoGPEWSxy0+jFqJxEGDOFLTjk91D30bp6wS4esbfQHGpSrJSwKlZUe32J48PLtp4El6PxIPZJDzq5o/S+3jOTJ5ATeSfj3/Gaf9fT3EbO7+MHpRrOzpf9HEah0FlVCCBKxmANqzgT0UIogNbvxNwpXVqsFR6FUkBFEytA8EhQhpnmSrlCj+vDlxxszfS50PrZ1K345M9IvGvhnOxIXco/nc7/xhQ6e7zBnT+2Y4tavcOrt03E5N+hJ/DeSU= user@ubu-dev-ws"
 }
 
 # Create a new Ubuntu VM instance
 resource "aws_instance" "ubuntu_vm" {
   ami           = data.aws_ami.amazon_linux_ec2_ami.id
   instance_type = var.instance_type
-  key_name      = var.key_name
+  key_name      = aws_key_pair.deployer.key_name
   tags = {
     Name = "UbuntuVM"
   }
