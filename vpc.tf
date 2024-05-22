0fc7d503679c5c9b117a8218a2292e8550198fb2
sgtslaughta <richardsoto1010@gmail.com>
2024-05-22 13:12:51 -0400

    First run

--- vpc.tf
+++ vpc.tf
@@ -1,3 +1,43 @@
+variable "project_name" {
+  description = "Name of the project"
+  type        = string
+  default = "test"
+}
+
+variable "ec2_kp_pub_cert" {
+  description = "EC2 public key certificate"
+  type        = string
+}
+
+variable "dev_vpc_cidr_block" {
+  description = "dev_vpc_cidr_block"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidr_blocks" {
+  description = "Available CIDR blocks for public subnets"
+  type        = list(string)
+  default = [
+    "10.0.1.0/24",
+    "10.0.2.0/24",
+    "10.0.3.0/24",
+    "10.0.4.0/24"
+  ]
+}
+
+variable "private_subnet_cidr_blocks" {
+  description = "Available CIDR blocks for private subnets"
+  type        = list(string)
+  default = [
+    "10.0.101.0/24",
+    "10.0.102.0/24",
+    "10.0.103.0/24",
+    "10.0.104.0/24"
+  ]
+}
+
+
 resource "aws_vpc" "dev_vpc" {
   cidr_block           = var.dev_vpc_cidr_block
   enable_dns_hostnames = true
